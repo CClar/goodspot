64627f09e7f24fe81745703346dbb488c496a029
Clarence Chan <cchan@live.ca>
2018-09-22 13:35:44 -0400

    FilterBar componenet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import BookingForm from './components/BookingForm';
+import FilterBar from './components/FilterBar';
+
 class App extends Component {
   render() {
     return (
     <div>
-      <BookingForm/>
+      <FilterBar/>
     </div>
     );
   }
