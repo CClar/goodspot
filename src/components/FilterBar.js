e6df7a5bcd5314a948a873ad9838b149bf5b93c9
Clarence Chan <cchan@live.ca>
2018-09-22 14:20:11 -0400

    FilterBar componenet

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -5,18 +5,21 @@ export default class FilterBar extends Component {
         super(props);
         this.state = { value: '' };
     }
-    handleChange = (event)=>{
+    handleChange = (event) => {
         // TODO: send the states to list to be filtered, 
         // parsed also by filtered component
     }
     render() {
         return (
             <div>
-                <label>
-                    Distance
-<                   input type="text" name="name" />
-                </label>
-            {/* TODO: Add list component here */}
+                <label>Distance<input type="text" name="distance" /></label>
+                <form name="priceRange">
+                    <label>Low<input type="text" name="distance" /></label>
+                    <label>High<input type="text" name="distance" /></label>
+                </form>
+                <label>Froms<input type="time" id="time" name="times"/></label>
+                <label>To<input type="time" id="time" name="times"/></label>
+                <label>Date<input type="date" id="time" name="times"/></label>
             </div>
         );
     }
