9677b6d6afd282d81f7b9779983e9dff56dff8b1
FainTkl <fxcence@gmail.com>
2018-09-22 14:23:58 -0400

    ListPosting trouble shoot

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import  Search from './components/Search.js'
 import './App.css';
+import Listposting from './components/Listposting.js';
 
 class App extends Component {
   render() {
     return (
     <div>
       <Search/>
+      <Listposting/>
     </div>
     );
   }
--- src/components/Listposting.js
+++ src/components/Listposting.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
 
 export default class Listposting extends Component {
-  render() {
+  constructor(props){
+      this.state = {
+        year: "",
+        month: "",
+        day: ""
+      }
+  }
+
+  componentDidMount() {
+     users = ()=>{
+    {contact.map(contact=> (
+        <Contact
+        year = {contact.year}
+        month = {contact.month}
+        day = {contact.day}   
+        />
+    ))
+   } 
+  }
+
+   
+
+  
+
+    render() {
+        const { contact } = this.state;
     return (
       <div>
-        
+          {users}
+         }
       </div>
-    )
-  }
+    );
 }
+
+
