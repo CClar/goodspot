c8e32848e4877098a959567a0744724e1afff33b
William To <towilliam03@gmail.com>
2018-09-23 03:12:17 -0400

    Add LargePosting

--- src/components/PostingList/PostingList.js
+++ src/components/PostingList/PostingList.js
@@ -1,41 +1,87 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import './PostingList.css';
 
 export default class PostingList extends Component {
   constructor(props){
     super(props);
     this.state = {
+      distanceFromLocation: 2000, // in meters
       postings: []
     }
   }
 
   componentDidMount() {
+    this.fetchPostings();
+  }
+  
+  fetchPostings() {
     const { address } = this.props.location.state;
+    // const address = '4800 Yonge Street';
     const url = 'https://calm-basin-51790.herokuapp.com/testdistance';
-    
+    console.log(typeof this.state.distanceFromLocation);
+
     fetch(url, {
       method: 'POST',
-      body: { address },
-      mode: 'cors'
+      body: JSON.stringify({
+        address: address,
+        distanceFromLocation: this.state.distanceFromLocation
+      }),
+      headers: {"Content-Type": "application/json"},
+      mode: 'cors',
     })
-    .then((response) =>  response.json())
+    .then((response) =>  {
+      return response.json();
+    })
     .then((data) => {
       this.setState({
         postings: data
-      });
-      // console.log(data);
+      })
     })
     .catch((error) => {
       console.log('Request failed', error);
     });
   }
 
+  handleFilterSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      distanceFromLocation: parseInt(this.distanceInput.value, 10)
+    }, () => {
+      this.fetchPostings();
+    });
+  }
+
   render() {
-    const postings = this.state.postings;
+    const { address } = this.props.location.state;
+    console.log('length -- ' + this.state.postings.length);
     return (
       <div className="PostingList">
+        <h1 className="user-address">{address}</h1>
+        <div className="list-filter">
+          Distance from location: <input type="number" ref={(node) => {this.distanceInput = node}}></input> meters
+          <button onClick={this.handleFilterSubmit}>Refresh</button>
+        </div>
         <ul>
-          {postings.map((posting, i) => {
-            return <li key={i}>{posting.Address}</li>
+          {this.state.postings.map((posting, i) => {
+            const fromTime = (new Date(posting.FromTime)).getHours();
+            const toTime = (new Date(posting.ToTime)).getHours();
+            
+            return (
+              <li key={i}>
+                <Link to={{
+                  pathname: '/posting',
+                  state: {
+                    post: posting
+                  }
+                }} className="posting">
+                  <h2>{posting.Address}</h2>
+                  <p>Price: ${posting.Price.toFixed(2)}</p>
+                  <p>Availability:<br></br>{fromTime} - {toTime}</p>
+                </Link>
+              </li>
+            );
           })}
         </ul>
       </div>
--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import PostingList from './components/PostingList/PostingList';
+import LargePosting from './components/LargePosting/LargePosting';
 
 ReactDOM.render(
     <HashRouter>
         <Switch>
             <Route exact path='/' component={App}>></Route>
             <Route path='/postings' component={PostingList}>></Route>
+            <Route path='/posting' component={LargePosting}>></Route>
         </Switch>
     </HashRouter>,
     document.getElementById('root')
